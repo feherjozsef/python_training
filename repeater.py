ca021f7b2677da627dbbe53135878ae47eeeba45
feherjozsef <feher.jozsef92@gmail.com>
2022-03-02 16:18:18 +0100

    for tovabbi felhasznalas

--- repeater.py
+++ repeater.py
@@ -3,3 +3,10 @@ name=input("Add meg a neved")
 count=input("Hanyszor irjam ki a neved:") # bevitelkor is lehet integerre alakitani count=int(input("mivan"))
 for i in range(0,int(count)):       #for i in range(1, count+1): VAGY for i in range(count): ekkor default-ban 0-tol indul
     print(str(i+1) + " " + name)    #print(i+1, name)
+
+for name in ["john", "jack", "jane"]:   #listát nem legeneráltatom a range-gel hanem megadom konkrétan
+    print(name)
+
+for i in [2, 4, 6, 8, 10]:              #a ciklus változó felveszi a lista értékeit
+    print(i)
+
