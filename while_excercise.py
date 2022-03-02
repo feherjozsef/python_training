92340a10e564ea3ff47b3efa041a3da527fd8e6a
feherjozsef <feher.jozsef92@gmail.com>
2022-03-02 16:50:54 +0100

    while fejlesztes

---
+++ while_excercise.py
@@ -0,0 +1,12 @@
+#Kerjunk be a felhasznalotol szamot es a ketszereset kell kiírni, ha 0-at ir lepjen ki a program
+#Sajat megoldas
+szam=2
+while szam!=0:
+    szam=int(input("Adj meg egy szamot:"))
+    print(szam*2)
+
+#Masik megoldas
+szam=int(input("Adj meg egy szamot:"))
+while szam!=0:
+    print(szam*2)
+    szam = int(input("Adj meg egy szamot:"))
