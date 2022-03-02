f9975244094a29a31b65cd352352afca752d95cf
Résztvevő - 5 <T360-v2-DTPB-s05@oktatas.local>
2022-03-02 11:56:22 +0100

    Initial commit

---
+++ Variables.py
@@ -0,0 +1,33 @@
+print(5)
+print(100_000)
+
+print(3.14)
+
+print("World")
+print('asd')
+
+name = "Feher Jozsi"
+print(name)
+age = 30
+print(age)
+age = 35
+print(age)
+age=age+5
+print(age)
+color_of_eye="zöld"
+print(color_of_eye)
+color_of_eye_of_jon = color_of_eye + "kekes"
+print(color_of_eye_of_jon)
+print(type(age))
+age=age+3.14
+print(age)
+print(type(age))
+age = "harminckettő"
+print(type(age))
+print(type("asd"))
+
+salary: int =200
+print(salary)
+salary=salary+3.14
+print(salary)
+
---
+++ hello.py
@@ -0,0 +1,4 @@
+#Jozsi programja
+print("Hello World") #ctrl+d-vel duplikalhato a sor
+print("Hello World")
+print("Hello World")
