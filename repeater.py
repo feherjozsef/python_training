f5f905c785b4a1cfc4235d282e6429d8e9b416a6
feherjozsef <feher.jozsef92@gmail.com>
2022-03-02 16:15:29 +0100

    fejlesztes

--- repeater.py
+++ repeater.py
@@ -1,5 +1,5 @@
 #program gyak
 name=input("Add meg a neved")
-count=input("Hanyszor irjam ki a neved:")
-for i in range(0,int(count)):
-    print(str(i+1) + " " + name)
+count=input("Hanyszor irjam ki a neved:") # bevitelkor is lehet integerre alakitani count=int(input("mivan"))
+for i in range(0,int(count)):       #for i in range(1, count+1): VAGY for i in range(count): ekkor default-ban 0-tol indul
+    print(str(i+1) + " " + name)    #print(i+1, name)
