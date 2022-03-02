1137dcfd124c5d4a6ac99ba6947926ded1158824
feherjozsef <feher.jozsef92@gmail.com>
2022-03-02 15:30:19 +0100

    userinput gyakorlas

---
+++ user_interface.py
@@ -0,0 +1,9 @@
+# name=input("add meg a neved!")
+# print(name)
+
+number=input("Adj meg egy szamot es en megszorzom ottel!")
+print(int(number)*5)
+
+# print(int("abc")) hibauzenettel kilep
+print(int(3.14))
+
