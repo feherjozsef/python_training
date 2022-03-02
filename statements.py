3879f4c5348ff2d6f15cac00c862faf252dc7678
feherjozsef <feher.jozsef92@gmail.com>
2022-03-02 15:06:44 +0100

    kifejezesek gyakorlasa

---
+++ statements.py
@@ -0,0 +1,50 @@
+print(5 * 6)
+print(5 + 6)
+print(3 / 2)
+print(3 - 2)
+print(5 * 6 + 12)
+print(1 + 2 * 3)
+print((1 + 2) * 3)
+
+print("Alma" + "korte")
+
+#print(5+"string")
+#print("string"+5)
+#print("string" - "s")
+#print("string"*"msik")
+print("szoveg"*2)
+
+result = 6*5+2
+print(result)
+number_of_apples_per_basket = 5
+number_of_baskets = 3
+number_of_all_apples = number_of_apples_per_basket * number_of_baskets
+print(number_of_all_apples)
+
+name = "John Doa"
+message = "A nev valtozo tipusa" + str(type(name))
+print(message)
+
+print("Az almak szama:" + str(5))
+
+print(len("hello"))
+lenght_of_hello = len("hello")
+print(lenght_of_hello*5)
+
+hours = 3
+minutes = hours*60 # előző változó szorzása 60-nal
+print(str(hours) + " óra az " + str(minutes) + " perc")
+
+
+#hozzatok létre egy word nevű változót és adjátok neki értékül egy nagyon hosszú szót
+#számoljátok ki a hosszát
+# kiírni: a megszentsegtelenithetetlenseg xy karakter
+word = "megszentsegtelenithetetlenseg"
+print("A "+ "\"" + word+ "\""  + str(len(word)) + " karakterbol all")
+
+fruit = "alma"
+print('gyumolcs:"' + fruit + '"')
+print("gyumolcs:\"" + fruit + "\"")
+print(5//2)
+print(5/2)
+print(5%2)
