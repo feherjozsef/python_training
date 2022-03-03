a3aba4e00a98f683bdef655f7b216501a5432e63
feherjozsef <feher.jozsef92@gmail.com>
2022-03-03 11:57:42 +0100

    Szintaktikai hibak javitasa

--- number_thinking.py
+++ number_thinking.py
@@ -4,11 +4,10 @@ min_interval = 1
 max_interval = 10
 answer = "x"
 while answer != "e":
-    guess=(min_interval + max_interval)//2
+    guess = (min_interval + max_interval)//2
     answer = input("A következő számra tippelek: " + str(guess) + " Ez a helyes szám?")
     if answer == 'k':
         max_interval = guess-1
-    elif answer =='n':
+    elif answer == 'n':
         min_interval = guess+1
 print("A gondolt szám", guess)
-
