abaa2e25cc1f7e89ffb9aa0469c97ad440611354
feherjozsef <feher.jozsef92@gmail.com>
2022-03-03 09:24:02 +0100

    Day2 excercise

---
+++ sum.py
@@ -0,0 +1,13 @@
+#Lista tele számokkal és írjuk ki a számok összegét
+
+sum=0
+for i in [1, 2, 3, 4]:
+    sum=sum+i
+print(sum)
+
+sum=0
+list=[1, 2, 3]
+for i in list:
+    sum=sum+i   #sum+=i
+print(sum)
+print(type(list))
