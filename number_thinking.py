25cc58badd32a10f36a4285319376419f5ff99c0
feherjozsef <feher.jozsef92@gmail.com>
2022-03-03 11:47:52 +0100

    Binaris kereses

--- number_thinking.py
+++ number_thinking.py
@@ -1,13 +1,14 @@
+# Szam tippelos alkalmazas > Binaris kereses
 print("Gondolj egy számra 1 és 10 között")
 min_interval = 1
 max_interval = 10
 answer = "x"
 while answer != "e":
     guess=(min_interval + max_interval)//2
-    print("A", guess, "számra tippelek")
-    answer = input("Ez a helyes szám?")
+    answer = input("A következő számra tippelek: " + str(guess) + " Ez a helyes szám?")
     if answer == 'k':
         max_interval = guess-1
     elif answer =='n':
         min_interval = guess+1
 print("A gondolt szám", guess)
+
